fddef300713700ad260902784835f522dbb47425
Dmitry Shishkin <shishkindima@gmail.com>
2021-11-16 11:19:55 +0300

    refactor

--- src/main/java/algorithms/Main.java
+++ src/main/java/algorithms/Main.java
@@ -6,24 +6,51 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
+
     public static void main(String[] args) {
 
     }
 
     public static List<Integer> sortIncomingQueses(List<Queue<Integer>> list) {
         ArrayList<Integer> result = new ArrayList<>();
-        PriorityQueue<Integer> sortedElements = new PriorityQueue<>();
-        Integer element;
-        for (Queue<Integer> queue : list) {
-            while (!queue.isEmpty()) {
-                if ((element = queue.poll()) != null) {
-                    sortedElements.add(element);
+//        PriorityQueue<Integer> sortedElements = new PriorityQueue<>();
+//        Integer element;
+        boolean hasValues = true;
+        while (hasValues) {
+            int tempMin = Integer.MAX_VALUE;
+            int index = 0;
+            for (int i = 0; i < list.size(); i++) {
+                if (!list.get(i).isEmpty() && list.get(i).peek() < tempMin) {
+                    index = i;
+                    tempMin = list.get(i).peek();
                 }
             }
-        }
-        while (!sortedElements.isEmpty()) {
-            result.add(sortedElements.poll());
+            result.add(list.get(index).poll());
+            hasValues = checkQueue(list);
         }
         return result;
+
+//        for (Queue<Integer> queue : list) {
+//            while (!queue.isEmpty()) {
+//                if ((element = queue.poll()) != null) {
+//                    sortedElements.add(element);
+//                }
+//            }
+//        }
+//        while (!sortedElements.isEmpty()) {
+//            result.add(sortedElements.poll());
+//        }
+//        return result;
+    }
+
+    private static boolean checkQueue(List<Queue<Integer>> list) {
+        int count = 0;
+        for (Queue<Integer> queue : list) {
+            if (queue.isEmpty()){
+                count++;
+            }
+        }
+
+        return count != list.size();
     }
 }
